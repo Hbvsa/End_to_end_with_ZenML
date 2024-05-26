fad285485609549b8ab98ad921c5d4fe9eba7de5
hbvsa <nuclearrrrr@gmail.com>
2024-05-26 18:37:36 +0100

    updated chicken end to end project

--- Chicken_disease_project/main.py
+++ Chicken_disease_project/main.py
@@ -1,6 +1,7 @@
 from cnnClassifier import logger
 from cnnClassifier.pipeline import stage_01_data_ingestion
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline, STAGE_NAME
+from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline, STAGE_NAME as STAGE_NAME_2
 
 if __name__ == '__main__':
     try:
@@ -8,6 +9,12 @@ if __name__ == '__main__':
         obj = DataIngestionTrainingPipeline()
         obj.main()
         logger.info(f">>>> stage {STAGE_NAME} completed<<<<")
+
+        logger.info(f"*******************")
+        logger.info(f">>>> stage {STAGE_NAME_2} started <<<<")
+        obj = PrepareBaseModelTrainingPipeline()
+        obj.main()
+        logger.info(f">>>> stage {STAGE_NAME_2} completed <<<<")
 
     except Exception as e:
         logger.exception(e)
