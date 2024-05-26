b8e59d46526aee68a6a359a34cf8c3a4d7836a2f
hbvsa <nuclearrrrr@gmail.com>
2024-05-26 06:22:12 +0100

    end to end chicked update

--- Chicken_disease_project/main.py
+++ Chicken_disease_project/main.py
@@ -1,1 +1,16 @@
-from cnnClassifier
+from cnnClassifier import logger
+from cnnClassifier.pipeline import stage_01_data_ingestion
+from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline, STAGE_NAME
+
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>> stage {STAGE_NAME} started <<<<")
+        obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f">>>> stage {STAGE_NAME} completed<<<<")
+
+    except Exception as e:
+        logger.exception(e)
+        raise e
+
+
